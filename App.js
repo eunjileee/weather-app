387b9cf1ee6f22528f8bf9a9f8262add3bd74ea9
eunjileee <48424423+eunjileee@users.noreply.github.com>
2020-02-18 18:49:16 +0900

    weather app

--- App.js
+++ App.js
@@ -1,19 +1,45 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Alert } from 'react-native';
+import Loading from './Loading';
+import Weather from './Weather';
+import * as Location from 'expo-location';
+import axios from 'axios';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+
+const API_KEY = '79c061252b5891bca9b67be8cfa7e19c';
+
+
+export default class extends React.Component {
+  state = {
+    isLoading: true
+  }
+
+  getWeather = async (latitude, longitude) => {
+    const { data } = await axios.get(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`
+    );
+    this.setState({ isLoading: false, temp: data.main.temp });
+  };
+  
+  getLocation = async () => {
+    try {
+      await Location.requestPermissionsAsync();
+      const { 
+        coords: { latitude, longitude } 
+      } = await Location.getCurrentPositionAsync();
+      this.getWeather(latitude, longitude)
+      //Send to API and get weather!
+    } catch (error) {
+      Alert.alert("Can't find you.", "So sad");
+    }
+  };
+
+  componentDidMount() {
+    this.getLocation();
+  }
+
+  render() {
+    const { isLoading, temp } = this.state;
+    return isLoading ? <Loading /> : <Weather temp = {Math.round(temp)} />;
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ Loading.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
+
+
+export default function Loading () {
+    return (
+        <View style = {styles.container}>
+            <StatusBar barStyle = "dark-content" />
+            <Text style = {styles.text}>Getting the weather</Text>
+        </View>
+    )
+}
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'flex-end',
+        paddingHorizontal: 30,
+        paddingVertical: 100,
+        backgroundColor: '#9ed4f7',
+    },
+
+    text: {
+        color: 'white',
+        fontSize: 30,
+    }
+})
